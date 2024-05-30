21f00496f6948745757604858dfcaaf981ab3d4e
Eldar071 <eldar007nafikov@gmail.com>
2024-05-30 18:51:03 +0300

    Tested and data

---
+++ Tested_Data.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Tested {
+private:
+    vector<string> test;
+    int testResults;
+
+public:
+    friend class PersonalData;
+
+    void viewTest() {
+        for (const auto& question : test) {
+            cout << question << endl;
+        }
+    }
+
+    void registerTest() {
+    }
+    void workTest() {
+    }
+    void viewTestStats() {
+        cout << "Test results statistics: " << testResults << endl;
+    }
+};
+
+struct PersonalData {
+    string name;
+    string address;
+    string phone;
+};
+
+int main() {
+    Tested testInstance;
+
+    PersonalData person1;
+    person1.name = "";
+    person1.address = "";
+    person1.phone = "";
+
+    testInstance.viewTest();
+    testInstance.registerTest();
+    testInstance.workTest();
+    testInstance.viewTestStats();
+
+    return 0;
+}
