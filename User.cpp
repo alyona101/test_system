218c9d7d8df4855548caafc86267081c9998329c
Maria <masha8517@gmail.com>
2024-06-01 18:17:38 +0300

    first commit

---
+++ User.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+
+class User
+{
+public:
+	User(){}
+	virtual void Registration()
+	{
+		std::cout << "Registration of new user, create login and password";
+	}
+	virtual void Check_pass()
+	{
+		std::cout << "Check password of user";
+	}
+	virtual void Check_login()
+	{
+		std::cout << "Check login of user";
+	}
+	virtual void Encryption()
+	{
+		std::cout << "Encryption password of user";
+	}
+
+protected:
+
+
+};
+
